5fa0814c8cee5838da8bfc183a011cc17ecbd081
BrayanCortes123 <lolhola189@gmail.com>
2025-01-24 01:06:14 -0500

    cambioscinco

--- proyecto/server.js
+++ proyecto/server.js
@@ -6,11 +6,9 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Obtener la ruta del directorio utilizando import.meta.url
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Servir archivos estáticos (como tu HTML)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/precio-cafe', async (req, res) => {
@@ -20,6 +18,10 @@ app.get('/precio-cafe', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
