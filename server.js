acf23d56ca14b0213523120286f79593f394b13c
BrayanCortes123 <lolhola189@gmail.com>
2025-01-24 00:58:26 -0500

    cambioscuatro

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import path from 'path';
 import obtenerPrecioCafe from './index.js';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Obtener la ruta del directorio utilizando import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Servir archivos estáticos (como tu HTML)
 app.use(express.static(path.join(__dirname, 'public')));
